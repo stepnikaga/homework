0da4aca697c0acb0856559976f7297a3e4de4da6
Agnieszka <borkowska.a@gmail.com>
2021-06-26 13:24:09 +0200

    Moja appka

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Moja pierwsza apka</h1>
+    </div>
+  );
+}
+
+export default App;
